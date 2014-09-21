bf656b1079fc1ada67cfcfb3ec0455c4196972f2
Israel B. Sant'Anna <ibsa14@inf.ufpr.br>
2014-09-20 23:41:05 -0300

    Adicionado o main com o menu e um esqueleto do Cliente
    Signed-off-by: Israel B. Sant'Anna <ibsa14@inf.ufpr.br>

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	char *nome;
+	//Lista listaCompras;
+	char *revista;
+}Cliente;
+
+int main(){
+	puts("Bem vindo ao Supermercado Algorítmico!\n");
+	int opcao;
+	do{
+		puts("O que você gostaria de fazer:\n");
+		scanf("1. Adicionar Cliente\n2. Criar nova Lista de Compras\n3. Ir para a fila\n0. Sair\n",&opcao);
+	}while(opcao != 0);
+	return 0;
+}
