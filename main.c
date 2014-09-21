3150c602fc59e67d3b730c1519e53fbc1ce6486c
Vytor Calixto <vytorcalixto@gmail.com>
2014-09-21 20:27:27 -0300

    TAD Lista adicionado.

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tads/lista.c"
 
 typedef struct{
 	char *nome;
-	//Lista listaCompras;
+	Lista listaCompras;
 	char *revista;
 }Cliente;
 
---
+++ tads/lista.c
@@ -0,0 +1,57 @@
+// Lista duplamente encadeada
+typedef struct{
+	char elemento[]; //Para escrever strings
+	ElementoLista *proximo;
+	ElementoLista *anterior;
+}ElementoLista;
+
+typedef ElementoLista* Lista;
+
+void iniciaLista(Lista *lista){
+	lista = (Lista *) malloc(sizeof(Lista));
+	lista->proximo = lista;
+	lista->anterior = lista;
+}
+
+int vaziaLista(Lista *lista){
+	return (lista->proximo == lista);
+}
+
+// FIXME: Copiei o que estava no caderno, precisa de revisão
+void insereLista(Lista *lista, char[] elemento){
+	lista->anterior->proximo = (Lista *) malloc(sizeof(Lista));
+	lista->anterior->proximo->proximo = lista;
+	lista->anterior->proximo->anterior = lista->anterior;
+	lista->anterior = lista->anterior->proximo;
+	lista->anterior->elemento = elemento;
+}
+
+char* removeLista(Lista *lista, ElementoLista *posicao){
+	if(vaziaLista(lista)){
+		puts("Erro: não é possível remover.\nMotivo: a lista está vazia.");
+		return "";
+	}else if(posicao == lista){
+		puts("Erro: não é possível remover.\nMotivo: parâmetro posição aponta para nodo cabeça.");
+		return "";
+	}else{
+		posicao->anterior->proximo = posicao->proximo;
+		posicao->prox->anterior = posicao->anterior;
+		char elemento[] = posicao->elemento;
+		free(posicao);
+		return elemento;
+	}
+}
+
+void imprimeLista(Lista *lista){
+	if(vaziaLista(lista)){
+		puts("Erro: impossível imprimir lista.\nMotivo: a lista está vazia.");
+	}else{
+		ElementoLista aux = lista->proximo;
+		int valor = 1;
+		while(aux != lista){
+			printf("%d - %s", valor, aux->elemento);
+			aux = aux->proximo;
+			valor++;
+		}
+	}
+}
